03986d05954d80b065d792ca8abc8760f5a8628a
toremann <50668920+toremann@users.noreply.github.com>
2022-11-04 22:21:41 +0100

    Added Axios
    Promise based HTTP client for the browser and node.js
    
    https://www.npmjs.com/package/axios

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # NoroffDevelopmentTeam
 
 # Scripts
+
+```bash
 npm run dev (runs with nodemon)
 npm run format (prettier all files)
-
+```
--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const axios = require('axios');
 
 require('dotenv').config();
 
-// Initialise Express
+// Initialize Express
 const app = express();
+const port = 3000;
+const booksAPI = 'https://openlibrary.org/books/OL82586W.json';
 
 // Database URI from MongoDB Atlas
 const dbURI = process.env.MongoURI;
 
 // Connect to database and start Express if connection is successful
-mongoose.connect(dbURI)
-.then((result) => {
-    console.log("Mongoose connected to DB successfully");
-    app.listen(3000, function (err) {
-        if(err) console.log("Express failed to start: "+err)
-        console.log(`Express server is listening on port 3000`);
-    })
-})
-.catch((err) => console.log("Could not connect to database"));
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log('Mongoose connected to DB successfully');
+    app.listen(port, function (err) {
+      if (err) console.log('Express failed to start: ' + err);
 
-app.get('/', (req, res) => {
-    res.send('Hello, World');
-})
+      console.log(`Express running on ${port}`);
+    });
+  })
+  .catch((err) => console.log('Could not connect to database'));
+
+// Get book using axios
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(booksAPI);
+    res.json(response.data);
+    console.log(`Book title: ${response.data.title}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
